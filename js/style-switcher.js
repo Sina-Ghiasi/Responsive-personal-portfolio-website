05fe7eb9430d5c162c11a34e72d4f40f8c885cdd
Sina <sinasteyn@gmail.com>
2022-07-04 19:33:11 +0430

    add dark mode improve color names

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -23,3 +23,17 @@ const setActiveColor = function (color) {
     document.querySelector(`.${color}`).classList.add("color-selected");
   });
 };
+//theme light and dark mode
+const dayNight = document.querySelector(".day-night");
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
